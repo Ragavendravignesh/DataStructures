8ffce5336b7bc9b65b2d6212ea8958e14d937a07
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-22 14:19:10 +0530

    Stack using LinkedList added

---
+++ Stacks/StackLinkedList.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.bottom = null
+    this.top = null
+    this.length = 0
+  }
+
+  push(value) {
+    const newNode = new Node(value)
+    if (this.length === 0) {
+      this.bottom = newNode
+      this.top = newNode
+      this.length++
+    } else {
+      const holdingPointer = this.top; 
+      this.top = newNode;
+      this.top.next = holdingPointer;
+      this.length++
+    }
+  }
+
+  pop() {
+      if(!this.top) {
+          return null;
+      }
+
+      if(this.top === this.bottom) {
+          this.bottom = null;
+      }
+
+      this.top = this.top.next;
+      this.length--;
+  }
+
+  peek() {
+    return this.top
+  }
+}
+
+const myStack = new Stack();
+myStack.push('Madhan');
+myStack.push('Govind');
+myStack.push('Palani');
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack);
