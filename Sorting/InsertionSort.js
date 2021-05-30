ebe632849ba73c1263044de36f0554260a5b04fd
Ragavendra S <ragavendrabest77@gmail.com>
2021-05-30 09:37:21 +0530

    Insertion sort added

---
+++ Sorting/InsertionSort.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function InsertionSort(array) {
+    const length = array.length;
+    for(let i=0; i < length; i++) {
+        for( let j=0; j< length; j++) {
+            if( array[i] < array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+console.log(InsertionSort(numbers));
--- Sorting/SelectionSort.js
+++ Sorting/SelectionSort.js
@@ -3,7 +3,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 function selectionSort (array) {
     const length = array.length;
 
-    for(let i =0; i < length; i++ ){
+    for(let i =0; i < length; i++ ) {
         let min = i;
         for( let j= i+1; j < length; j++ ) {
             if(array[j] < array[min]) 
